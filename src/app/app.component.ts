a32a57682574751b1247c754c5a83a43e9ad23c3
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-03-17 13:35:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+
+  posts = [
+    { title: 'Neat Tree', 
+      imageUrl: 'assets/tree.jpeg', 
+      username: 'nature',
+      content: 'Olha que árvore massa' 
+    },
+    { title: 'Montanhas nevadas', 
+      imageUrl: 'assets/biking.jpeg', 
+      username: 'biking', 
+      content: 'Pedalando para manter saúde' 
+    },
+    { title: 'Pedalando de boas', 
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainGirl', 
+      content: 'Corram para as colinas' }
+  ];
+
 }
